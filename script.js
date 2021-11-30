2a7458e5e44bbca095b241c8aedbf04594d52b84
ZaidebinUsama <77499086+usama-khalied@users.noreply.github.com>
2021-11-30 15:44:12 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 const header = document.querySelector('header');
+const nav = document.querySelector('nav');
 function changeBackground() {
     if(window.scrollY >= 20){
         header.style.backgroundColor="white";
+        header.style.top="0";
+        // nav.style.margin="0 0";
     }
     else {
         header.style.backgroundColor="transparent";
+        // nav.style.margin="3rem 3rem";
+        header.style.top="5.5rem";
 
     }
 }
